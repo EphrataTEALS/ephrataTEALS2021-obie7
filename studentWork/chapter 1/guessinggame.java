5bd58038e9480650ea8900383241e4513641cd54
obie7 <munozhob000@ephrataschools.org>
2021-12-17 16:53:48 +0000

    save working

--- studentWork/chapter 1/guessinggame.java
+++ studentWork/chapter 1/guessinggame.java
@@ -6,10 +6,10 @@ public class guessinggame {
 
 public static void main(String [] args ){
 Random rannumber = new Random();
-    Scanner userchoice = new Scanner (System.in);
+    Scanner randnumber = new Scanner (System.in);
     System.out.print("choose a random number: ");
-   Int compchoice  = randnumber.nextInt(61);
-
+   int compchoice  = randnumber.nextInt(5);
+   
 
     
     int guess;
@@ -17,11 +17,12 @@ Random rannumber = new Random();
 
     while (!correct){
         System.out.print( "Guess: ");
+        Scanner userchoice = new Scanner(System.in);
         guess = userchoice.nextInt();
         
         if (guess == compchoice) {
             correct= true ;
-            Systm.out.println("you are right ");
+            System.out.println("you are right ");
         }
         else if (guess < compchoice){
             System.out.println("Higher");
